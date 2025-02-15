f5af75598fed62279d32c3e69c87c91ba66784d5
jamesLaurino <petersthomas90@gmail.com>
2025-02-15 22:28:07 +0100

    implementation of JpaRepository,findProductById, findAllProduct and security.

--- src/main/resources/db/DML/data-001.sql
+++ src/main/resources/db/DML/data-001.sql
@@ -23,3 +23,13 @@ insert into supplier_entity (id,address_id,product_id,registration_number) value
 
 insert into comment_entity (id,header,body,create_at,client_id) values(1,'Congratulation','Hello there',now(),null);
 insert into comment_entity (id,header,body,create_at,client_id) values(2,'Bad Luck','Good by',now(),null);
+
+--changeset tpe:2
+insert into client_entity(id,comment_id,address_id,is_active,created_at,updated_at,email,password,username)values(1,1,1,true,now(),now(),'thomas@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','thomas');
+insert into role_entity (id,name) values(1,'ROLE_ADMIN');
+insert into user_roles (role_id,user_id) values(1,1);
+
+--changeset tpe:3
+insert into client_entity(id,comment_id,address_id,is_active,created_at,updated_at,email,password,username)values(1,2,2,true,now(),now(),'james@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','james');
+insert into role_entity (id,name) values(1,'ROLE_USER');
+insert into user_roles (role_id,user_id) values(1,1);
