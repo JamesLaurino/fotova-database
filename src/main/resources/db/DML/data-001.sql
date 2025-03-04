31d77facfd8cd57b61f17893e2c6078192867bd3
jamesLaurino <petersthomas90@gmail.com>
2025-03-04 15:11:23 +0100

    FTO-0027 retrieve all clients and refactoring db

--- src/main/resources/db/DML/data-001.sql
+++ src/main/resources/db/DML/data-001.sql
@@ -21,15 +21,16 @@ insert into supplier_entity (id,address_id,product_id,registration_number) value
 insert into supplier_entity (id,address_id,product_id,registration_number) values(3,5,3,'123457');
 insert into supplier_entity (id,address_id,product_id,registration_number) values(4,5,4,'123457');
 
-insert into comment_entity (id,header,body,create_at,update_at,client_id) values(1,'Congratulation','Hello there',now(),now(),null);
-insert into comment_entity (id,header,body,create_at,update_at,client_id) values(2,'Bad Luck','Good by',now(),now(),null);
-
 --changeset tpe:2
-insert into client_entity(id,comment_id,address_id,is_active,created_at,updated_at,email,password,username)values(1,1,1,true,now(),now(),'thomas@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','thomas');
+insert into client_entity(id,address_id,is_active,created_at,updated_at,email,password,username)values(1,1,true,now(),now(),'thomas@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','thomas');
 insert into role_entity (id,name) values(1,'ROLE_ADMIN');
 insert into user_roles (role_id,user_id) values(1,1);
 
 --changeset tpe:3
-insert into client_entity(id,comment_id,address_id,is_active,created_at,updated_at,email,password,username)values(2,2,2,true,now(),now(),'james@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','james');
+insert into client_entity(id,address_id,is_active,created_at,updated_at,email,password,username)values(2,2,true,now(),now(),'james@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','james');
 insert into role_entity (id,name) values(2,'ROLE_USER');
 insert into user_roles (role_id,user_id) values(2,2);
+
+--changeset tpe:4
+insert into comment_entity (id,header,body,create_at,update_at,client_id) values(1,'Congratulation','Hello there',now(),now(),1);
+insert into comment_entity (id,header,body,create_at,update_at,client_id) values(2,'Bad Luck','Good by',now(),now(),1);
