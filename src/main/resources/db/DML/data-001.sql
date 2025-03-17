38fd5f113199fad697df39022b1ce77d36e73e44
jamesLaurino <petersthomas90@gmail.com>
2025-03-17 16:44:08 +0100

    FTO-0002 Refactoring columns names in db

--- src/main/resources/db/DML/data-001.sql
+++ src/main/resources/db/DML/data-001.sql
@@ -24,12 +24,12 @@ insert into supplier_entity (id,address_id,product_id,registration_number) value
 --changeset tpe:2
 insert into client_entity(id,address_id,is_active,created_at,updated_at,email,password,username)values(1,1,true,now(),now(),'thomas@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','thomas');
 insert into role_entity (id,name) values(1,'ROLE_ADMIN');
-insert into client_roles (role_id,client_id) values(1,1);
+insert into client_role_entity (role_id,client_id) values(1,1);
 
 --changeset tpe:3
 insert into client_entity(id,address_id,is_active,created_at,updated_at,email,password,username)values(2,2,true,now(),now(),'james@hotmail.com','$2a$10$VCtb0afUP.nWv2YOGjJ0Sedci7HKV/trVZDDIGXNUFa33/UyhKILa','james');
 insert into role_entity (id,name) values(2,'ROLE_USER');
-insert into client_roles (role_id,client_id) values(2,2);
+insert into client_role_entity (role_id,client_id) values(2,2);
 
 --changeset tpe:4
 insert into comment_entity (id,header,body,created_at,updated_at,client_id) values(1,'Congratulation','Hello there',now(),now(),1);
