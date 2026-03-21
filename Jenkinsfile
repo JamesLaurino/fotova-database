b23f744eb75097a39ea40f7056ec42eac5d998bf
jamesLaurino <petersthomas90@gmail.com>
2026-03-21 12:53:41 +0100

    FTO-158 fixing pipeline jenkins databse adding adding create table and refactoring changelog-master.xml

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,6 +6,14 @@ pipeline {
         maven 'Maven-3.9'
     }
 
+    parameters {
+        choice(
+            name: 'LIQUIBASE_TARGET',
+            choices: ['tables', 'data'],
+            description: 'Choisir le changelog Liquibase à lancer'
+        )
+    }
+
     stages {
 
         stage('Checkout') {
@@ -20,27 +28,31 @@ pipeline {
             }
         }
 
-        stage('Liquibase Migration') {
-
+        stage('Liquibase Update') {
             steps {
+                script {
+                    def changelogFile = ''
 
-                withCredentials([
+                    if (params.LIQUIBASE_TARGET == 'tables') {
+                        changelogFile = 'src/main/resources/db/changelog-master-create.xml'
+                    } else if (params.LIQUIBASE_TARGET == 'data') {
+                        changelogFile = 'src/main/resources/db/changelog-master.xml'
+                    }
 
-                    string(credentialsId: 'SERVER_HOST', variable: 'SERVER_HOST'),
-                    string(credentialsId: 'DB_NAME', variable: 'DB_NAME'),
-                    string(credentialsId: 'DB_USERNAME', variable: 'DB_USERNAME'),
-                    string(credentialsId: 'DB_PASSWORD', variable: 'DB_PASSWORD')
-
-                ]) {
-
-                    sh """
-                    mvn liquibase:update \
-                      -Dliquibase.url=jdbc:mysql://$SERVER_HOST:3306/$DB_NAME \
-                      -Dliquibase.username=$DB_USERNAME \
-                      -Dliquibase.password=$DB_PASSWORD \
-                      -Dliquibase.changeLogFile=src/main/resources/db/changelog-master.xml
-                    """
-
+                    withCredentials([
+                        string(credentialsId: 'DB_HOST', variable: 'DB_HOST'),
+                        string(credentialsId: 'DB_NAME', variable: 'DB_NAME'),
+                        string(credentialsId: 'DB_USERNAME', variable: 'DB_USERNAME'),
+                        string(credentialsId: 'DB_PASSWORD', variable: 'DB_PASSWORD')
+                    ]) {
+                        sh """
+                        mvn liquibase:update \
+                          -Dliquibase.url=jdbc:mysql://$DB_HOST:3306/$DB_NAME \
+                          -Dliquibase.username=$DB_USERNAME \
+                          -Dliquibase.password=$DB_PASSWORD \
+                          -Dliquibase.changeLogFile=${changelogFile}
+                        """
+                    }
                 }
             }
         }
--- src/main/resources/db/DDL/creation-001.sql
+++ src/main/resources/db/DDL/creation-001.sql
@@ -1,91 +1,147 @@
 --liquibase formatted sql
 
 --changeset tpe:1
-CREATE TABLE t_category (
-    id integer not null PRIMARY KEY,
-    name varchar(255)
+create table address_entity
+(
+    id      int auto_increment
+        primary key,
+    city    varchar(255) null,
+    country varchar(150) null,
+    number  varchar(20)  null,
+    street  varchar(255) null
 );
 
-CREATE TABLE t_role (
-      id integer not null PRIMARY KEY,
-      name ENUM ('ROLE_USER', 'ROLE_ADMIN')
+create table category_entity
+(
+    id   int auto_increment
+        primary key,
+    name varchar(255) null
 );
 
-CREATE TABLE t_comment (
-    id integer not null PRIMARY KEY,
-    header varchar(255),
-    body varchar(255),
-    createAt timestamp
+create table client_entity
+(
+    id         int auto_increment
+        primary key,
+    created_at datetime(6)  null,
+    email      varchar(255) not null,
+    is_active  bit          null,
+    password   varchar(255) null,
+    updated_at datetime(6)  null,
+    username   varchar(255) null,
+    address_id int          null,
+    constraint UKkmppj0slc23iap61bqoil9wk6
+        unique (email),
+    constraint FKrycawy7tdhg2uymsn4ysqhbhc
+        foreign key (address_id) references address_entity (id)
 );
 
-CREATE TABLE t_address (
-    id integer not null PRIMARY KEY,
-    street varchar(255),
-    city varchar(255),
-    number varchar(20),
-    country varchar(150)
+create table comment_entity
+(
+    id         int auto_increment
+        primary key,
+    body       varchar(255) null,
+    created_at datetime(6)  null,
+    header     varchar(255) null,
+    updated_at datetime(6)  null,
+    client_id  int          null,
+    constraint FK89uco3i6ylu07r76bni3v3irx
+        foreign key (client_id) references client_entity (id)
 );
 
-CREATE TABLE t_product (
-    id integer not null PRIMARY KEY,
-    name varchar(255),
-    quantity integer,
-    price numeric,
-    category_id integer,
-
-    FOREIGN KEY (category_id) REFERENCES t_category(id)
+create table order_entity
+(
+    id        int auto_increment
+        primary key,
+    create_at datetime(6) null,
+    is_done   bit         null,
+    client_id int         null,
+    constraint FKr6wyb1ksam8lrmmsrxet9xouf
+        foreign key (client_id) references client_entity (id)
 );
 
-CREATE TABLE t_supplier (
-    id integer not null PRIMARY KEY,
-    registration_number varchar(150),
-    address_id integer,
-    product_id integer,
-
-    FOREIGN KEY (address_id) REFERENCES t_address(id),
-    FOREIGN KEY (product_id) REFERENCES t_product(id)
+create table product_entity
+(
+    id          int auto_increment
+        primary key,
+    description varchar(255) null,
+    name        varchar(255) null,
+    price       double       null,
+    quantity    int          null,
+    url         varchar(255) null,
+    category_id int          null,
+    constraint FK8kxxmqdokh3lthvw0t148w0bc
+        foreign key (category_id) references category_entity (id)
 );
 
-CREATE TABLE t_client (
-    id integer not null PRIMARY KEY,
-    username varchar(255),
-    email varchar(255),
-    password varchar(255) unique not null,
-    isActive BOOLEAN,
-    createdAt timestamp,
-    updatedAt timestamp,
-
-    address_id integer,
-    comment_id integer,
-
-    FOREIGN KEY (address_id) REFERENCES t_address(id),
-    FOREIGN KEY (comment_id) REFERENCES t_comment(id)
+create table image_entity
+(
+    id         int auto_increment
+        primary key,
+    path       varchar(255) null,
+    product_id int          null,
+    constraint FKrna9so4bjernm1yis2t27g99x
+        foreign key (product_id) references product_entity (id)
 );
 
-CREATE TABLE t_role_client (
-    id integer not null PRIMARY KEY,
-    role_id integer,
-    client_id integer,
-
-    FOREIGN KEY (role_id) REFERENCES t_role(id),
-    FOREIGN KEY (client_id) REFERENCES t_client(id)
+create table label_entity
+(
+    id             int auto_increment
+        primary key,
+    description_en varchar(255) null,
+    description_fr varchar(255) null,
+    description_ru varchar(255) null,
+    title_en       varchar(255) null,
+    title_fr       varchar(255) null,
+    title_ru       varchar(255) null,
+    product_id     int          null,
+    constraint UK997xr1cruurhk9lu4iia9mp0a
+        unique (product_id),
+    constraint FKt2qrqkttyohuejtcb0km19iw1
+        foreign key (product_id) references product_entity (id)
 );
 
-CREATE TABLE t_order (
-    id integer not null PRIMARY KEY,
-    createAt timestamp,
-    client_id integer,
-
-    FOREIGN KEY (client_id) REFERENCES t_client(id)
+create table order_product_entity
+(
+    id               int auto_increment
+        primary key,
+    quantity_product int null,
+    order_id         int null,
+    product_id       int null,
+    constraint FK6c2ka8wdi4ex825504smlmbys
+        foreign key (product_id) references product_entity (id),
+    constraint FKhsxw661ao11rdxc1pkclbptn8
+        foreign key (order_id) references order_entity (id)
 );
 
-CREATE TABLE t_order_product (
-    id integer not null PRIMARY KEY,
-    quantity_product integer,
-    order_id integer,
-    product_id integer,
-
-    FOREIGN KEY (order_id) REFERENCES t_order(id),
-    FOREIGN KEY (product_id) REFERENCES t_product(id)
-
+create table role_entity
+(
+    id   int auto_increment
+        primary key,
+    name enum ('ROLE_ADMIN', 'ROLE_USER') null
 );
+
+create table client_role_entity
+(
+    client_id int not null,
+    role_id   int not null,
+    primary key (client_id, role_id),
+    constraint FK3o8arngdjhearngbm162c455i
+        foreign key (role_id) references role_entity (id),
+    constraint FKgverq2163rxgo1798wckphxfo
+        foreign key (client_id) references client_entity (id)
+);
+
+create table supplier_entity
+(
+    id                  int auto_increment
+        primary key,
+    registration_number varchar(150) null,
+    address_id          int          null,
+    product_id          int          null,
+    constraint FK4hr98q6f3iu0wuyo1e7ku325v
+        foreign key (product_id) references product_entity (id),
+    constraint FKdc7aoci2itm4ybc160dur3kka
+        foreign key (address_id) references address_entity (id)
+);
+
+
